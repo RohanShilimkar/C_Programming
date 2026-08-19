3002685a0e8d5ab8cd633e1d01b83cf5bb66301c
RohanShilimkar <rohanshilimka15@gmail.com>
2026-08-19 17:42:24 +0530

    Add files via upload

---
+++ Competitive/Assignment_44/program2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void Display(int No)
+{
+    static int i=1;
+
+    if(i<=No)
+    {
+        printf(" %d ",i);
+        i++;
+        Display(No);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number:");
+    scanf("%d",&iValue);
+    Display(iValue);
+
+    return 0;
+}
---
+++ Competitive/Assignment_44/program4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Display(int No)
+{
+    static char ch = 'A';
+
+    if(No>0)
+    {
+        printf(" %c ",ch);
+        ch++;
+        No--;
+        Display(No);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number:");
+    scanf("%d",&iValue);
+    Display(iValue);
+
+    return 0;
+}
